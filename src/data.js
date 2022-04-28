6f66768e86f70b2409f7a2142ecc88a4c91a8d92
Marco Person <marcoperson2000@gmail.com>
2022-04-28 19:02:29 +0200

    Correction des bugs dues a AOS

--- src/data.js
+++ src/data.js
@@ -2,37 +2,37 @@ export const projectsData = [
     {
         id : 0,
         img : "/assets/portfolio.png",
-        link : "#",
+        link : "https://marco-portfolio.pages.dev/",
         alt : "My Portfolio Image"        
     },
     {
         id : 1,
         img : "/assets/ecommerce.png",
-        link : "#",
+        link : "https://marco-ecommerceapp.pages.dev/",
         alt : "My Ecommerce Image"        
     },
     {
         id : 2,
         img : "/assets/meteo.png",
-        link : "#",
+        link : "https://marco-meteoapp.pages.dev/",
         alt : "My Meteo Image"        
     },
     {
         id : 3,
         img : "/assets/calculator.png",
-        link : "#",
+        link : "https://marco-calculatorapp.pages.dev/",
         alt : "My Calculator Image"        
     },
     {
         id : 4,
         img : "/assets/game.png",
-        link : "#",
+        link : "https://marco-basicgameproject.pages.dev/",
         alt : "My Game Image"        
     },
     {
         id : 5,
         img : "/assets/github.png",
-        link : "#",
+        link : "https://github.com/MarcoPerson",
         alt : "My Github Image"        
     }
 ]
