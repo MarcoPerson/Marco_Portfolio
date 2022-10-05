1740af565e58a8878f56c9da5ff31d2fd19406e0
Merveilles AGBETI-MESSAN <marcoperson2000@gmail.com>
2022-10-05 13:51:21 +0200

    Adding linkedin button

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,56 +1,72 @@
-import React, { useContext, useEffect } from 'react';
-import { ThemeContext } from '../darkcontext';
-import './About.css';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import React, { useContext, useEffect } from "react";
+import { ThemeContext } from "../darkcontext";
+import "./About.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function About(props) {
-    const theme = useContext(ThemeContext);
+  const theme = useContext(ThemeContext);
 
-    useEffect(() => {
-        AOS.init({duration: 2000});
-    },[])
+  useEffect(() => {
+    AOS.init({ duration: 2000 });
+  }, []);
 
-    return (
-        <div className='about' data-aos="fade-left">
-            <div className="about-left">
-                <div className="about-left-wrapper">
-                    <div className="about-bg" style={{backgroundColor: theme.state.darkMode && "#ccc"}}>
-
-                    </div>
-                    <div className="about-image">
-                        <img src="/assets/computer.jpg" alt="Group Work" className="about-imagepic" />
-                    </div>
-                </div>
-            </div>
-            <div className="about-right">
-                <div className="about-right-wrapper">
-                    <div className="about-card-upper">
-                        <h2 className='about-title'>About Me</h2>
-                        <div className="about-short">
-                        I'm a 4th year student at INSA Toulouse, currently doing Computer Science and Networks
-                        </div>
-                        <div className="about-desc">
-                        My interests are in web and mobile development, data, cybersecurity and mathematics. I am autonomous, rigorous and dynamic. I like working in a team and learning new technologies.
-                        </div>
-                    </div>
-                    <div className="about-card-lower">
-                        <div className="award-image">
-                            <img src="/assets/icpc.png" alt="" className="award-imagepic" />
-                        </div>
-                        <div className="award-body">
-                            <h4 className="award-title">
-                                Second place in the programming contest
-                            </h4>
-                            <div className="award-text">
-                                Being also the first ones to solve problem D, we got two prizes.
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="about" data-aos="fade-left">
+      <div className="about-left">
+        <div className="about-left-wrapper">
+          <div
+            className="about-bg"
+            style={{ backgroundColor: theme.state.darkMode && "#ccc" }}
+          ></div>
+          <div className="about-image">
+            <img
+              src="/assets/computer.jpg"
+              alt="Group Work"
+              className="about-imagepic"
+            />
+          </div>
         </div>
-    );
+      </div>
+      <div className="about-right">
+        <div className="about-right-wrapper">
+          <div className="about-card-upper">
+            <h2 className="about-title">About Me</h2>
+            <div className="linkedin-container">
+              <a
+                className="linkedin-button"
+                href="https://www.linkedin.com/in/merveilles-agbeti-messan-3bb03b1ba/"
+              >
+                <button>LinkedIn Account</button>
+              </a>
+            </div>
+            <div className="about-short">
+              I'm a 4th year student at INSA Toulouse, currently doing Computer
+              Science and Networks
+            </div>
+            <div className="about-desc">
+              My interests are in web and mobile development, data,
+              cybersecurity and mathematics. I am autonomous, rigorous and
+              dynamic. I like working in a team and learning new technologies.
+            </div>
+          </div>
+          <div className="about-card-lower">
+            <div className="award-image">
+              <img src="/assets/icpc.png" alt="" className="award-imagepic" />
+            </div>
+            <div className="award-body">
+              <h4 className="award-title">
+                Second place in the programming contest
+              </h4>
+              <div className="award-text">
+                Being also the first ones to solve problem D, we got two prizes.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
