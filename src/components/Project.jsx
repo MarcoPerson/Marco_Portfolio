739e90b55b2ed033e0ad006cf65fc2bc9e46367e
Merveilles AGBETI-MESSAN <marcoperson2000@gmail.com>
2022-10-05 13:41:41 +0200

    Update: Project - Dark Mode for Project List

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,16 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext } from 'react';
 import Tag from './Tag';
 import './Project.css';
 import AOS from 'aos';
+import { ThemeContext } from "../darkcontext";
 import 'aos/dist/aos.css';
 
 function Project({data}) {
+    const theme = useContext(ThemeContext);
     useEffect(() => {
         AOS.init({duration: 2000});
     },[])
     return (
-        <div className='project' data-aos="fade-left">
-            <div className="project-style">
+        <div className='project' style={{borderColor: theme.state.darkMode ? "black" : "#f3f2f2"}} data-aos="fade-left">
+            <div className="project-style" style={{backgroundColor: theme.state.darkMode ? "black" : "#f3f2f2"}}>
                 <div className="circle"></div>
                 <div className="circle"></div>
                 <div className="circle"></div>
@@ -18,7 +20,7 @@ function Project({data}) {
             <a href={data.link} className="imagewrapper">
                 <img src={data.img} alt={data.alt} />
             </a>
-            <div className='tools-list'>
+            <div className='tools-list' style={{backgroundColor: theme.state.darkMode ? "black" : "#f3f2f2"}}>
                 {data.tools.map((item) => <Tag>{item}</Tag>)}
             </div>
         </div>
