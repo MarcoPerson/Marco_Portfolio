8a1189efc1d983984c98d597acef3af05d5597db
Merveilles AGBETI-MESSAN <marcoperson2000@gmail.com>
2026-04-07 13:58:52 +0200

    Fix: Remove unused Cursus import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { useContext, useEffect } from "react";
 import "./App.css";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import Cursus from "./components/Cursus";
 import Header from "./components/Header";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
